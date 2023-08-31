7adc412f16725666a20dd701c77eb8d737a65436
Frut0sS <alexfrutos8@gmail.com>
2023-08-31 18:41:56 +0200

    1.0

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Alejandro Frutos | Personal Portfolio';
+export const SITE_DESCRIPTION = 'Alejandro Frutos Full Stack Developer';
